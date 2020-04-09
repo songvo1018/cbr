aeb914ac06b8180637909d34e2fc0ac8473a5c3e
Leha Song Vo <52455378+songvo1018@users.noreply.github.com>
2020-04-10 00:07:20 +0700

    Version 5.0
    Rebuild markup, style
    Added weighting

--- main.js
+++ main.js
@@ -1,103 +1,97 @@
 function calculate() {
-  let p = {
-    size: parseFloat(document.getElementById('size').value),
-    width: parseFloat(document.getElementById('width').value),
-    thick: parseFloat(document.getElementById('thick').value),
-    color: document.getElementById('color').value,
-    profile: document.getElementById('profile').checked,
+	let data = {
+		size: parseFloat(document.getElementById("size").value),
+		width: parseFloat(document.getElementById("width").value),
+		thick: parseFloat(document.getElementById("thick").value),
+		color: document.getElementById("color").value,
+		profile: document.getElementById("profile").checked,
   };
-  let sizeCoef = (function (size, thick) {
-    if (thick > 1.7) {
-      return ((size + thick) * 3.14) + 0.4;
-    } else if (thick == 1.2) {
-      return ((size + (thick - 0.2)) * 3.14)
-    } else {
-      return (size + thick) * 3.14;
-    }
-  })(p.size, p.thick);
-  let widthCoef = (function (sizeCoef, width) {
-    if (width > 6) {
-      return (sizeCoef + 0.4);
-    } else if (width > 4) {
-      return (sizeCoef + 0.2);
-    } else if (width < 4 && width > 2) {
-      return sizeCoef;
-    } else {
-      return (sizeCoef - 0.3);
-    }
-  })(sizeCoef, p.width);
-  let colorCoef = (function (widthCoef, color) {
-    if (color === 'White') {
-      return widthCoef + 0.3;
-    } else if (color === 'Red') {
-      return widthCoef;
-    } else if (color === 'Lemon') {
-      return widthCoef - 0.25;
-    }
-  })(widthCoef, p.color);
-  let profileCoef = (function (colorCoef, profile) {
-    if (profile) {
-      return colorCoef - 0.2
-    } else {
-      return colorCoef;
-    }
-  })(colorCoef, p.profile);
-  let res = {
-    length: profileCoef.toFixed(2),
-    size: p.size,
-    width: p.width,
-    thick: p.thick,
-    color: p.color,
-    profile: p.profile
+  
+	let sizeCoef = (function (size, thick) {
+		if (thick > 1.7) {
+			return (size + thick) * 3.14 + 0.4;
+		} else if (thick == 1.2) {
+			return (size + (thick - 0.2)) * 3.14;
+		} else {
+			return (size + thick) * 3.14;
+		}
+  })(data.size, data.thick);
+  
+	let widthCoef = (function (sizeCoef, width) {
+		if (width > 6) {
+			return sizeCoef + 0.4;
+		} else if (width > 4) {
+			return sizeCoef + 0.2;
+		} else if (width < 4 && width > 2) {
+			return sizeCoef;
+		} else {
+			return sizeCoef - 0.3;
+		}
+  })(sizeCoef, data.width);
+  
+	let colorCoef = (function (widthCoef, color) {
+		if (color === "White") {
+			return widthCoef + 0.3;
+		} else if (color === "Red") {
+			return widthCoef;
+		} else if (color === "Lemon") {
+			return widthCoef - 0.25;
+		}
+  })(widthCoef, data.color);
+  
+	let profileCoef = (function (colorCoef, profile) {
+		if (profile) {
+			return colorCoef - 0.2;
+		} else {
+			return colorCoef;
+		}
+  })(colorCoef, data.profile);
+  
+	let weight = profileCoef.toFixed(2) * data.thick * data.width * 0.013;
+  
+	let res = {
+		length: profileCoef.toFixed(2),
+		size: data.size,
+		width: data.width,
+		thick: data.thick,
+		color: data.color,
+    profile: data.profile,
+    weight: weight.toFixed(2)
   };
+  
+  
 
-  function createRes(obj) {
-    let rs = document.getElementById('result');
+	function createRes(obj) {
+		let rs = document.getElementById("result");
 
-    let res = document.createElement('res');
-    res.className = "res";
-    res.setAttribute('id', 'detail-show')
-    res.innerHTML = `${obj.length}, Color: ${obj.color} <br>`;
-
-    let det = document.createElement('det');
-    det.className = "detail";
-    det.setAttribute('id', 'detail-hide')
-    det.innerHTML = `Size: ${obj.size}, Thick: ${obj.thick}, Width: ${obj.width},  Half-rounded: ${obj.profile} <br>`
+		let res = document.createElement("res");
+    res.innerHTML = `
+      <p>${obj.length}, 
+      Color: ${obj.color} ,
+      Size: ${obj.size}, <br>
+      Thick: ${obj.thick},
+      Width: ${obj.width}, <br>
+      &#8978 ${obj.profile} <br>
+      Weight: ${obj.weight}</p>`;
+    
     rs.append(res);
-
-    function showDetail() {
-      res.append(det);
-
-      function hideDetail() {
-        det.setAttribute('class', 'hide');
-        console.log('yes')
-      };
-
-      let hide = document.getElementById('detail-hide');
-      if (hide) {
-        document.querySelector('#detail-hide').addEventListener('click', hideDetail);
-      };
-    };
-
-    let show = document.getElementById('detail-show');
-    if (show) {
-      document.querySelector('#detail-show').addEventListener('click', showDetail);
-    }
-
-  }
-  createRes(res);
-  console.log(res)
+		
+	}
+	createRes(res);
+	console.log(res);
 }
-let go = document.getElementById('calculate');
+
+let go = document.getElementById("calculate");
 if (go) {
-  document.querySelector('#calculate').addEventListener('click', calculate);
+	document.querySelector("#calculate").addEventListener("click", calculate);
 }
 
 function deleteResult() {
-  document.getElementById('result').innerHTML = " ";
+	document.getElementById("result").innerHTML = " ";
 }
-// let res = document.createElement('res');
-// res.className = "result hide";
-// res.innerHTML = ``;
 
-// document.body.append(res);
+function clearInput() {
+    document.getElementById("size").value = ''
+		document.getElementById("width").value = '' 
+    document.getElementById("thick").value = ''
+}
